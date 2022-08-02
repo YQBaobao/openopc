331e49b3dc72eb7baa11817ef8adfd6f5f34d635
yqbao <yqbaowo@qq.com>
2022-08-02 21:22:59 +0800

    打包 V1.3.3

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     maintainer_email='yqbaowo@foxmail.com',
     name="OpenOPC-Python3x",
     packages=find_packages(),
-    py_modules=['OpenOPC'],
     python_requires='>=3.8',
     url='https://github.com/YQBaobao/openopc',
     version="1.3.3",
